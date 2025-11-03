41fcdc9fd63834fe4b0c313045709dc8a3c9d1c5
Bambam-coder123 <virgilholder23@gmail.com>
2025-11-02 21:25:15 -0500

    Initial Commit

---
+++ iOSApp4/iOSApp4App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp4App.swift
+//  iOSApp4
+//
+//  Created by Mac User on 2025-11-02.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSApp4Tests/iOSApp4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp4Tests.swift
+//  iOSApp4Tests
+//
+//  Created by Mac User on 2025-11-02.
+//
+
+import Testing
+@testable import iOSApp4
+
+struct iOSApp4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
